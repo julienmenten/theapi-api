05c0be6df6e768ea0cd25019f8bb7b6538dab529
Julien <menten.julien@outlook.be>
2020-12-20 18:13:55 +0100

    Develop(server.test.js): Wrote new tests for all endpoints in complete flow

--- api/src/__tests__/server.test.js
+++ api/src/__tests__/server.test.js
@@ -6,48 +6,45 @@ const request = supertest(app)
 
 describe(' GET /api', ()=> {
     test('responds with 200', async (done) => {
-         const response = await request.get('/api')
+         const response = await request.get('/apis')
          expect(response.status).toBe(200, done())
        
     })
 })
 
-
-describe(' Fetch the data of an API submitted by the user', () => {
-    test('Responds with the contect of the API', async (done) => {
-        let testAPI = "https://api.chucknorris.io/jokes/random";
-            expect(fetchNewAPIData(testAPI)).toBe(!undefined, done());
-    })
-
-    test('Responds with an error', async (done) => {
-        expect(fetchNewAPIData(testAPI)).toBe(!undefined, done());
-    })
-})
-
-
 describe('Create and POST a new API in the database, then remove that entry', () => {
-    test('POST a new record to the DB', async (done) => {
+
+    let UUID = "";
+    test('POST a new entry to the DB', async (done) => {
         const TEST_API = {
             api_url: "https://jsonplaceholder.typicode.com/todos/1",
             api_name: "JSON placeholder test API",
             allowed_endpoints: ["GET", "POST", "PUT", "PATCH", "DELETE"],
             description: "Free to use fake online REST API for testing and prototyping."
         }
+
+        const response = await request.post('/apis').send(TEST_API)
+        expect(response.status).toBe(200, done())
+
     })
 
-    test('GET that record from the DB', async (done) => {
-
+    test('GET the new entry from the DB', async (done) => {
+        const response = await request.get(`/apis/${UUID}`)
+        expect(resonse.status).toBe(200, done())
     })
 
     test('UPDATE the name of the entry', async (done) => {
-
+        const response = await request.patch(`/apis/${UUID}`)
+        expect(response.status).toBe(200, done())
     })
 
-    test('GET the updated record from the DB', async (done) => {
-
+    test('GET the updated entry from the DB', async (done) => {
+        const response = await request.get(`/apis/${UUID}`)
+        expect(response.status).toBe(200, done())
     })
 
     test('DELETE the entry', async (done) => {
-        
+        const response = await request.delete(`/apis/${UUID}`)
+        expect(response.status).toBe(200, done())
     })
 })
