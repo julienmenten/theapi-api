a5eaf1bb56e48a4c89578a5d44ac8f55e4ea35c7
Julien <menten.julien@outlook.be>
2020-12-20 20:33:33 +0100

    Develop(server.js): Added comments to functions to help clarify code

--- api/src/server.js
+++ api/src/server.js
@@ -6,6 +6,8 @@ const fetch = require('node-fetch');
 const { json } = require('body-parser');
 const {v4: uuidv4} = require('uuid')
 
+/*
+ */
 
 const pg = require('knex')({
     client: 'pg',
@@ -18,8 +20,17 @@ const pg = require('knex')({
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extented: true}))
 
+/*
+ =================================================================
+            API ENDPOINTS
+ =================================================================
+*/
+/*
+GET /apis
+  Gets all the records from the 'api' Database
 
-// GET all API's 
+  TODO (optional): Add pagination
+*/
 app.get('/apis', async (req, res) => {
     let apiList = []
     await pg('api').select('*').then( apis => {
@@ -31,7 +42,10 @@ app.get('/apis', async (req, res) => {
 });
 
 
-// FIND ONE API 
+/*
+GET /apis/:uuid
+    Finds the API in the databse that matches the provided UUID
+*/
 app.get('/apis/:id', async (req, res) => {
     console.log
     const uuid = req.params.id;
@@ -39,7 +53,6 @@ app.get('/apis/:id', async (req, res) => {
     const result = await pg('api')
         .select('*')
         .where({uuid: uuid})
-
 
     if(result.length >= 1) {
         res.status(200).send({
@@ -52,12 +65,16 @@ app.get('/apis/:id', async (req, res) => {
 });
 
 
-/* POST request for a new API 
-        Accepts:
-            API-name: String
-            API-link: String
-            Allowed-endpoints: String[]
-            Description: String
+/* 
+POST /apis
+    Adds a new API to the database.
+    The body of the request must contain:
+        API-name: String
+        API-link: String
+        Allowed-endpoints: String[]
+        Description: String
+    
+    TODO: Add security in case the request does not have the required inputs
 */
 app.post('/apis' , (req, res) => {
 
@@ -82,6 +99,87 @@ app.post('/apis' , (req, res) => {
     }
    
 })
+
+/* 
+DELETE /apis/:uuid
+    Deletes a record from the database that matches the provided UUID
+*/
+app.delete('/apis/:uuid', async (req, res) => {
+    const uuid = req.params.uuid
+    try {
+        await pg('api').where({uuid: uuid}).del().returning('*').then(data => {
+            console.log(`API deleted! Goodbye`)
+            res.status(200).send()
+        })
+        
+    }catch(e){
+        console.log(e)
+        res.status(400).send("An unknown error has occured")
+    }
+    
+})
+
+/* 
+PATCH /apis/:uuid
+   Updates the content of a specific API that matches the provided UUID. 
+   The body can be any attributes from the original object: 
+        API-name: String
+        API-link: String
+        Allowed-endpoints: String[]
+        Description: String
+
+        TODO: Secure the endpoint so that you get error messages 
+*/
+
+ app.patch('/apis/:uuid', async (req, res) => {
+    const uuid = req.params.uuid;
+    const newBody = req.body
+    pg('api').where({uuid: uuid}).update(newBody).returning('*').then(data => {
+        console.log(data)
+        res.status(200).send()
+    })
+ })
+
+/*
+ =================================================================
+        FUNCTIONS MANIPULATING DATA
+ =================================================================
+*/
+
+/* 
+Fetches the API provided at POST and uses that data to find the 
+    Input: 
+        URL: string
+*/
+async function fetchNewAPIData(URL) {
+    try{
+        await fetch(URL, {method: 'GET'})
+        .then(res => {
+            let apiContent = res.json();
+            console.log(apiContent)
+            return  apiContent;
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+/* 
+Analyzes a record and its attributes. Then creates a list from the attributes it found and their type. 
+This list is then returned as an array containing objects. 
+    Input: 
+        data: 1 result object from the fetched API 
+
+    Output:   
+        attribute: {
+            attribute_name: String
+            attribute_type: String
+        }[]
+    
+*/
+function formatProperties(data){
+    
+}
+
 /*
     InsertAPI() function accepts an object with data comming from the POST request and inserts the new API record to the database.
     This function also searches for the properties from the API makes a readable JSON format to put in the database. 
@@ -101,55 +199,11 @@ async function insertAPI(data){
     })
 }
 
-// DELETE request of an API
-app.delete('/apis/:uuid', async (req, res) => {
-    const uuid = req.params.uuid
-    try {
-        await pg('api').where({uuid: uuid}).del().returning('*').then(data => {
-            console.log(`API deleted! Goodbye`)
-            res.status(200).send()
-        })
-        
-    }catch(e){
-        console.log(e)
-        res.status(400).send("An unknown error has occured")
-    }
-    
-})
-
-// PATCH request of an API
-// TODO: Secure the endpoint so that you get error messages 
- app.patch('/apis/:uuid', async (req, res) => {
-    const uuid = req.params.uuid;
-    const newBody = req.body
-    pg('api').where({uuid: uuid}).update(newBody).returning('*').then(data => {
-        console.log(data)
-        res.status(200).send()
-    })
- })
-
-/* Fetch the info of the new API 
-    Accepts: 
-        URL: string
+/*
+ =================================================================
+        FUNCTIONS MANIPULATING THE DATABASE
+ =================================================================
 */
-async function fetchNewAPIData(URL) {
-    try{
-        await fetch(URL, {method: 'GET'})
-        .then(res => {
-            let apiContent = res.json();
-            console.log(apiContent)
-            return  apiContent;
-        })
-    }catch(e){
-        console.log(e)
-    }
-}
-
-function formatProperties(){
-
-}
-// Initializing the API table
-
 /*
 For PostgreSQL, due to incompatibility between native array and json types, 
 when setting an array (or a value that could be an array) as the value of a json or jsonb column, 
@@ -163,10 +217,12 @@ you should use JSON.stringify() to convert your value to a string prior to passi
 /*
     Checks the database and it's tables. In case an 'api' table does not exist, it creates a new table that follows the knex schema. 
     This knex schema contains; 
-        - an increment: int
-        - an uuid: uuid
-        - a name: String
-        - properties: json
+        - id (increment): int
+        - uuid: uuid
+        - api_name: String
+        - api_url: String
+        - description: String
+        - properties: String[]
         - endpoints: json
         - timestamps: Timestamps
 */
@@ -198,10 +254,15 @@ async function initialiseTables() {
 
 /* 
     ResetTables function resets the table in case a column has been changed, added or removed. 
+    ! ONLY USE WHEN A RESET OF THE TABLE IS NEEDED
 */
 async function resetTables(){
     await pg.schema.dropTableIfExists('api');
 }
+
+/*
+    Table functions
+*/
 initialiseTables()
 
 module.exports = app;
